4abc0d9ae5cf0a347392f9b77bd90f8308367656
“dtnguynCORRECT_EMAIL=“adron0914@gmail.com <>
2021-03-11 11:25:25 -0500

    Build ui for problem item

---
+++ mobile/components/deviceDetail/StatsBox.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import CustomText from "../util/CustomText";
+
+interface Props {
+  value: number;
+  title: string;
+  color: "green" | "yellow";
+}
+
+const StatsBox: React.FC<Props> = ({ title, value, color }) => {
+  return (
+    <View
+      style={{
+        ...styles.container,
+        backgroundColor: color === "green" ? "#62D67B" : "#EEF227",
+      }}
+    >
+      <CustomText fontSize={13}>{value}</CustomText>
+      <CustomText style={styles.text} fontSize={10}>
+        {title}
+      </CustomText>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    aspectRatio: 1,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  text: {
+    position: "absolute",
+    bottom: 2,
+  },
+});
+
+export default StatsBox;
